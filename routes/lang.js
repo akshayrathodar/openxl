1dfe90e475708d58d5ab44e08aaede77fe14a4cf
Dell <akshayrathod975@gmail.com>
2021-05-04 22:38:16 +0530

    first commit

---
+++ routes/lang.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const todos = require('../service/lang');
+
+/* GET programming languages. */
+router.get('/', async function(req, res, next) {
+  try {
+    res.json(await todos.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`error in fetching todo `, err.message);
+    next(err);
+  }
+});
+
+
+router.post('/', async function(req, res, next) {
+    try {
+      res.json(await todos.create(req.body));
+    } catch (err) {
+      console.error(`Error while creating todo`, err.message);
+      next(err);
+    }
+});
+
+
+router.post('/update', async function(req, res, next) {
+    try {
+      res.json(await todos.update(req.body.id, req.body));
+    } catch (err) {
+      console.error(`Error while updating programming language`, err.message);
+      next(err);
+    }
+});
+
+router.post('/delete', async function(req, res, next) {
+    try {
+      res.json(await programmingLanguages.remove(req.body.id));
+    } catch (err) {
+      console.error(`Error while deleting programming language`, err.message);
+      next(err);
+    }
+});
+
+module.exports = router;
---
+++ service/lang.js
@@ -0,0 +1,79 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getMultiple(page = 1){
+  
+  const rows = await db.query(
+    `SELECT * FROM todo `
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+async function create(resbody){
+    const result = await db.query(
+      `INSERT INTO todo 
+      (todotext , status)
+      VALUES 
+      (?, ?)`, 
+      [
+        resbody.todotext, resbody.status
+      ]
+    );
+  
+    let message = 'Error in creating programming language';
+  
+    if (result.affectedRows) {
+      message = 'Todo created successfully';
+    }
+  
+    return {message};
+}
+
+async function update(id, resbody){
+    const result = await db.query(
+      `UPDATE todo
+      SET todotext=?, status=? 
+      WHERE id=?`, 
+      [
+        resbody.todotext, resbody.status,id
+      ]
+    );
+  
+    let message = 'Error in updating todo';
+  
+    if (result.affectedRows) {
+      message = 'todo updated successfully';
+    }
+  
+    return {message};
+}
+
+
+async function remove(id){
+    const result = await db.query(
+      `DELETE FROM todo WHERE id=?`, 
+      [id]
+    );
+  
+    let message = 'Error to delete todo';
+    if (result.affectedRows) {
+      message = 'todo deleted successfully';
+    }
+    return {message};
+}
+  
+
+
+module.exports = {
+  getMultiple,
+  create,
+  update,
+  remove
+}
